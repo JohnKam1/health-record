63299cdd21a7788debace6c8c1a09524670c3668
Hootsybit <shiyunbao@topease.net>
2025-08-26 11:10:09 +0800

    feat(back): 添加定时任务功能

--- back/src/main/java/com/Hootsybit/HealthRecordApplication.java
+++ back/src/main/java/com/Hootsybit/HealthRecordApplication.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class HealthRecordApplication implements CommandLineRunner {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(HealthRecordApplication.class);
@@ -18,5 +21,10 @@ public class HealthRecordApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         LOGGER.info("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+
+    @Scheduled(cron = "0 */2 * * * ?")
+    public void schedule () {
+         LOGGER.info("(♥◠‿◠)ﾉﾞ  定时任务启动   ლ(´ڡ`ლ)ﾞ  \n");
     }
 }
